f57dd95b3c71971e13ddd04775d63b5b4e9d4e6c
FRYoussef <f.r.youssef@hotmail.com>
2022-04-07 13:45:37 +0200

    WIP: changing cuda code for dimensionality feature

--- cuda/main.cu
+++ cuda/main.cu
@@ -7,28 +7,29 @@
 #include "device/device.cuh"
 
 int main(int argc, const char* argv[]) {
-    if (argc < 4) {
-        std::cerr << "usage: k_means <data-file> <k> <number-points> [iterations]" << std::endl;
+    if (argc < 5) {
+        std::cerr << "usage: k_means <data-file> <clusters> <dimensions> <number-points> [iterations]" << std::endl;
         std::exit(EXIT_FAILURE);
     }
 
-    const auto k = std::atoi(argv[2]);
-    const auto n_points = std::atoi(argv[3]);
-    const auto number_of_iterations = (argc == 5) ? std::atoi(argv[4]) : 300;
+    const auto clusters             = std::atoi(argv[2]);
+    const auto dims                 = std::atoi(argv[3]);
+    const auto n_points             = std::atoi(argv[4]);
+    const auto number_of_iterations = (argc == 6) ? std::atoi(argv[5]) : 300;
 
-    std::vector<float> h_x, h_y;
+    std::vector<float> h_attrs(n_points*dims);
     std::ifstream stream(argv[1]);
     std::string line;
-    for(int i{n_points}; std::getline(stream, line) && i > 0; i--) {
+    for(int i{0}; std::getline(stream, line) && i < n_points; i++) {
         std::istringstream line_stream(line);
-        float x, y;
-        uint16_t label;
-        line_stream >> x >> y >> label;
-        h_x.push_back(x);
-        h_y.push_back(y);
+        float val;
+        for(int j{0}; j < dims; j++) {
+            line_stream >> val;
+            h_attrs[i + n_points*j] = val;
+        }
     }
 
-    Device device = Device(k, h_x, h_y);
+    Device device = Device(clusters, dims, n_points, h_attrs);
 
     const auto start = std::chrono::high_resolution_clock::now();
     device.run_k_means(number_of_iterations);
@@ -36,12 +37,14 @@ int main(int argc, const char* argv[]) {
     const auto duration = std::chrono::duration_cast<std::chrono::duration<float>>(end - start);
     std::cerr << "Took: " << duration.count() << "s" << std::endl;
 
-    std::vector<float> mean_x(k, 0);
-    std::vector<float> mean_y(k, 0);
-    device.save_solution(mean_x, mean_y);
+    std::vector<float> mean(clusters*dims, 0);
+    device.save_solution(mean);
 
-    for (size_t cluster = 0; cluster < k; ++cluster)
-        std::cout << mean_x[cluster] << " " << mean_y[cluster] << std::endl;
+    for (size_t cluster{0}; cluster < clusters; ++cluster) {
+        for(size_t d{0}; d < dims; d++)
+            std::cout << mean[cluster * dims + d] << " ";
+        std::cout << std::endl;
+    }
 
     return 0;
 }
