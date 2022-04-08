150e775af2a56cf90a00646b56dc39e4e40a3ec4
FRYoussef <f.r.youssef@hotmail.com>
2022-04-08 09:23:59 +0200

    minnor issue

--- sycl/device/device.cpp
+++ sycl/device/device.cpp
@@ -212,6 +212,7 @@ template <typename T>
 void Device::_cpu_reduce(T* vec, size_t dims, size_t dim_offset) { 
     _queue.submit([&](handler &h) {
         int attrs_size = this->attribute_size;
+        int k = this->k;
 
         h.single_task([=]() {
             for (int cluster = 0; cluster < k; ++cluster) { 
